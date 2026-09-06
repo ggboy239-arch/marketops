c74f10bf893a943d6a440af71f99de6626c099e4
ggboy239-arch <ggboy239@gmail.com>
2026-09-05 19:52:19 -0700

    v0.5 load news cog

--- bot.py
+++ bot.py
@@ -46,6 +46,7 @@ async def load():
     await bot.load_extension("cogs.market")
     await bot.load_extension("cogs.coach")
     await bot.load_extension("cogs.explain")
+    await bot.load_extension("cogs.news")
 
 
 # --------------------
