ec80e994c2957825b516815251832619a3d38fd5
ggboy239-arch <ggboy239@gmail.com>
2026-09-06 19:33:22 -0700

    Sync status version to v2.4.4

--- cogs/status.py
+++ cogs/status.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from market.watchlist_engine import WatchlistEngine
 
 
-VERSION = "MarketOps v2.4.1"
+VERSION = "MarketOps v2.4.4"
 PT_ZONE = ZoneInfo("America/Los_Angeles")
 
 
